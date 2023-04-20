252317fb0b3a60f623ce8f4822cfdecce0d06dc2
Bange boss <bangegeorgeogeto@gmail.com>
2023-04-20 17:30:36 +0300

    First commit

---
+++ src/main/java/com/metropolitan/childrenservice/ChildrenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.childrenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/childrenservice/ChildrenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.childrenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
